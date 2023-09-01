befb84193ced28e34b78e7b148ee3adfa920cbda
Aryan Nagar <AryanNagar@fico.com>
2023-09-01 17:50:06 +0530

    changes

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
-# !/bin/bash
-yum update
 yum install -y docker
 usermod -a -G docker ec2-user
 systemctl start docker.service
 docker pull aryannagar/testapache
-docker build -t test-apache --build-arg PORT=8000 .
-docker run -e "USERNAME=aryan" -e "PASSWORD=1234" -d -p 8000:8000 test-apache
+docker run -e "USERNAME=aryan" -e "PASSWORD=1234" -d -p 8000:8000 aryannagar/testapache
